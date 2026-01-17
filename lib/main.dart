e88f9672e7ba37dee234dcef99b0c01275bcd55f
deepak1945 <62013682+deepak1945@users.noreply.github.com>
2026-01-18 02:04:51 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,7 @@ import 'package:flutter/material.dart';
 import 'injection/injection_container.dart' as di;
 import 'my_app.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await di.init();
-  runApp(
+Future<void> 
     const MyApp()
   );
 }
