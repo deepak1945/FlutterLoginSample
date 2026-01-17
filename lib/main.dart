7725c468fceba71f08bc3ab89b9940af784edad7
deepak1945 <62013682+deepak1945@users.noreply.github.com>
2026-01-18 02:13:09 +0530

    Restore main entrypoint

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'injection/injection_container.dart' as di;
 import 'my_app.dart';
 
-Future<void> 
-    const MyApp()
-  );
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(const MyApp());
 }
